9396eaf79b28122e62023649145377fa1928d4cf
Sergio <s.olmedo13@hotmail.com>
2016-12-30 18:23:19 +0100

    formulario graficas

---
+++ FuelOptimizer/Graficas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FuelOptimizer
+{
+    public partial class Graficas : Form
+    {
+        public Graficas()
+        {
+            InitializeComponent();
+        }
+    }
+}
